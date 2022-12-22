3d810004794289d1434e5ed38ab9f8e1ab13e854
Sofia Niyazson <92554135+sofianiyazson@users.noreply.github.com>
2022-12-23 00:17:23 +0100

    JS count solution

--- js/count.js
+++ js/count.js
@@ -1,15 +1,14 @@
-
-
 // Skriv en funktion som räknar antalet tecken
 // som anges som parameter till funktionen
 
 function count(string) {
   // Skriv din kod här inne
 
+  return string.length;
 }
 
 // Ändra inte dessa
-const result = count("ABCDEFG123")
+const result = count('ABCDEFG123');
 console.log(result); // 10
 
 module.exports = count;
