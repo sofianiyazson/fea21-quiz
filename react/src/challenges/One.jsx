895b3c833bf2a0a9d6b6786f3ad3520a8f7dcd78
Sofia Niyazson <92554135+sofianiyazson@users.noreply.github.com>
2022-12-22 22:35:26 +0100

    react first challenge solved

--- react/src/challenges/One.jsx
+++ react/src/challenges/One.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react';
 
 // Gör så att "times" börjar på 0 och ökar med 1 varje gång du trycker på den.
 
 const One = () => {
-
+  const [times, setTimes] = useState(0);
 
   // Ta inte bort data-testid
   return (
     <div>
       <p data-testid="one-text">Has been clicked {times} times.</p>
-      <button data-testid="one-button">Click me</button>
+      <button data-testid="one-button" onClick={() => setTimes(times + 1)}>
+        Click me
+      </button>
     </div>
   );
 };
