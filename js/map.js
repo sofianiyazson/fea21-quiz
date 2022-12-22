0a5ad636470745594e0696960d086839b7714810
Sofia Niyazson <92554135+sofianiyazson@users.noreply.github.com>
2022-12-23 00:37:03 +0100

    object solution

--- js/map.js
+++ js/map.js
@@ -1,5 +1,3 @@
-
-
 // Skriv en funktion som tar den givna arrayen
 // och julifierar den genom att lägga till "Jul"
 // framför allt.
@@ -7,10 +5,10 @@
 
 function christmasify(array) {
   // Skriv din kod här
-  
+  return array.map((item) => 'Jul' + item);
 }
 
-const result = christmasify(["Sill", "Julmust", "Korv", "Janssons", "Svill"])
-console.log(result) // ["JulSill", "JulJulmust", "JulKorv", "JulJanssons", "JulSvill"]
+const result = christmasify(['Sill', 'Julmust', 'Korv', 'Janssons', 'Svill']);
+console.log(result); // ["JulSill", "JulJulmust", "JulKorv", "JulJanssons", "JulSvill"]
 
 module.exports = christmasify;
--- js/object.js
+++ js/object.js
@@ -1,19 +1,18 @@
-
 // Rör ej
 const person = {
-  firstName: "Oscar",
-  lastName: "Nilsson",
-  age: 30
-}
+  firstName: 'Oscar',
+  lastName: 'Nilsson',
+  age: 30,
+};
 
 // Skriv klart funktionen som skriver ut mitt fulla namn
 // OBS: Du får inte ändra funktions-parametrarna.
 function printName(first, last) {
   // Skriv din kod här
-
+  return `${first} ${last}`;
 }
 
 // Du kommer även behöva ändra anropet här nedan
-printName(person.firstName, person.lastName) // "Oscar Nilsson"
+printName(person.firstName, person.lastName); // "Oscar Nilsson"
 
-module.exports = printName
+module.exports = printName;
