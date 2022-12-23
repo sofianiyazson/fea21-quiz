29e5d0b5c94ad220a1ae27c8ad5c3573faa19e19
Sofia Niyazson <92554135+sofianiyazson@users.noreply.github.com>
2022-12-23 02:22:05 +0100

    filter solved

--- js/filter.js
+++ js/filter.js
@@ -1,17 +1,20 @@
-
-
 // Skriv en funktion som tar bort räliga matvaror från
 // en given array och returnerar en ny array med acceptabel mat.
 // Använd dig av .filter()
 
-const nastyFoods = ["Pannkakor", "Lingonsylt", "Fläsksvålar"];
+const nastyFoods = ['Pannkakor', 'Lingonsylt', 'Fläsksvålar'];
 
-function noNastyFood(array) {
-  // Skriv din kod här
+const noNastyFood = (array) => {
+  return array.filter((food) => !nastyFoods.includes(food));
+};
 
-}
-
-const result = noNastyFood(["Korv", "Sylta", "Pannkakor", "Chips", "Lingonsylt"]);
+const result = noNastyFood([
+  'Korv',
+  'Sylta',
+  'Pannkakor',
+  'Chips',
+  'Lingonsylt',
+]);
 console.log(result); // ["Korv", "Sylta", "Chips"]
 
 module.exports = noNastyFood;
